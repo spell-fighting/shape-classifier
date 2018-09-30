27ed26d91fb1c99509eddc88b3fd491a3e270c33
Edouard Menayde <edouard.menayde@gmail.com>
2018-09-30 12:46:18 +0200

    Add test and train scripts + first models

--- test.py
+++ test.py
@@ -1,63 +1,58 @@
 from __future__ import print_function
-import keras
-from keras.datasets import mnist
 from keras.models import Sequential
+from keras.layers import Activation
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
+from keras.models import load_model
 from keras import backend as K
+from keras.preprocessing.image import img_to_array, load_img
+from keras.preprocessing.image import ImageDataGenerator
 
-batch_size = 128
-num_classes = 10
-epochs = 12
+from keras.backend.tensorflow_backend import set_session
+import tensorflow as tf
 
-# input image dimensions
-img_rows, img_cols = 28, 28
+config = tf.ConfigProto(
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
+    # device_count = {'GPU': 1}
+)
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+set_session(session)
 
-# the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# img = load_img("dataset/test/triangle/qjtspokgkzydrzv123iu.png")
+# x = img_to_array(img)
+# x = x.reshape((1,) + x.shape)
+#
+# img_width, img_height = 150, 150
+#
+# if K.image_data_format() == 'channels_first':
+#     input_shape = (3, img_width, img_height)
+# else:
+#     input_shape = (img_width, img_height, 3)
 
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+img_width, img_height = 150, 150
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+model = load_model('./models/model_0.h5')
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+test_datagen = ImageDataGenerator()
+validation_data_dir = 'dataset/validation'
+batch_size = 16
+classes = ["circle", "hourglass", "square", "star", "triangle"]
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
+test_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical',
+    classes=classes
+)
 
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+loss, acc = model.evaluate_generator(test_generator, verbose=1)
+
+print('\nTesting loss: {}, acc: {}\n'.format(loss, acc))
+
+# prediction = model.predict(x)
+#
+# print("Circle : {} \n Hourglass : {} \n Square : {} \n Star : {} \n Triangle : {} \n".format(prediction[0][0], prediction[0][1], prediction[0][2], prediction[0][3], prediction[0][4]))
+
---
+++ train.py
@@ -0,0 +1,107 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+
+import os
+
+from keras.backend.tensorflow_backend import set_session
+import tensorflow as tf
+
+config = tf.ConfigProto(
+    gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
+    # device_count = {'GPU': 1}
+)
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+set_session(session)
+
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+train_data_dir = 'dataset/train'
+validation_data_dir = 'dataset/validation'
+test_data_dir = 'dataset/test'
+nb_train_samples = 644
+nb_validation_samples = 540
+epochs = 50
+batch_size = 16
+classes = ["circle", "hourglass", "square", "star", "triangle"]
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(5))
+model.add(Activation('softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+    rotation_range=40,
+    shear_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+
+test_datagen = ImageDataGenerator()
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical',
+    classes=classes
+)
+
+print(train_generator.class_indices)
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical',
+    classes=classes
+)
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
+
+test_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical',
+    classes=classes
+)
+
+model.evaluate_generator(test_generator, verbose=1)
+
+model.save('./models/model_{}.h5'.format(len(next(os.walk("./models"))[2])))
